ae23a73afd907507457e29633c48018b75dcb7d8
20123231 <1070443499@qq.com>
2014-05-24 21:36:16 +0800

    fork of first0524

---
+++ fork0524/hello.c
@@ -0,0 +1,15 @@
+/*===================================================================================*/
+//*    Filename:  hello.c
+//*    Description:  
+//*    Version:  1.0
+//*    Created:  05/24/2014 09:35:14 PM
+//*    Revision:  none
+//*    Compiler&Usage:  gcc \nr to choose&add temp
+//*    Author:  Cai San (cs), 1070443499@qq.com
+//*    Company:  wanna
+/*====================================================================================*/
+#include<stdio.h>
+int main(){
+	printf ("hello world");
+	return 0;
+}
